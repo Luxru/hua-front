8f9f7031e911cbfc28dc048286f48602deaabbfe
lux <46088753+Luxru@users.noreply.github.com>
2023-04-23 00:29:48 +0800

    [dev] update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
-import App from "@/components/App.js";
+import Header from "@/components/Header";
+import Panel from "@/components/Panel";
+import Footer from "@/components/Footer";
 import Head from "next/head";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Hua App</title>
       </Head>
-      <App />
+      <div className="bg-slate-100">
+      <div className="bg-white min-h-screen w-4/5 grid grid-flow-row shadow-xl mx-auto overflow-auto rounded-2xl">
+        <div className="self-start">
+          <Header />
+        </div>
+        <div className="self-center">
+          <Panel />
+        </div>
+        <div className="self-end">
+          <Footer />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
